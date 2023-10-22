31e875d9009a060cab4035b2c2e5e7354d306592
YC <79392304+yeongchan1228@users.noreply.github.com>
2023-10-22 20:09:54 +0900

    PUR-21 sentry 작업 (#4)
    - profile=prod인 경우에만 에러 모니터링

---
+++ src/main/kotlin/gongback/pureureumserver/exception/ErrorCode.kt
@@ -0,0 +1,7 @@
+package gongback.pureureumserver.exception
+
+import org.springframework.http.HttpStatus
+
+enum class ErrorCode(val httpStatus: HttpStatus, val code: Int, val errorMessage: String) {
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, -10001, "Internal server error"),
+}
