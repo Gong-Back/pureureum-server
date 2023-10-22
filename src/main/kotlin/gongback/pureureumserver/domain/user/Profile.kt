a3fd24c6d0979c5c0b071dd907540e55eefc4d78
jiwon <ljwon77@daum.net>
2023-10-23 02:58:02 +0900

    PUR-9 사용자 관련 도메인 설계

---
+++ src/main/kotlin/gongback/pureureumserver/domain/user/Profile.kt
@@ -0,0 +1,34 @@
+package gongback.pureureumserver.domain.user
+
+import gongback.pureureumserver.support.domain.BaseTimeEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+
+@Entity
+class Profile(
+    fileKey: String,
+    contentType: String,
+    originalFileName: String
+) : BaseTimeEntity() {
+    @Column(nullable = false)
+    var fileKey: String = fileKey
+        protected set
+
+    @Column(nullable = false)
+    var contentType: String = contentType
+        protected set
+
+    @Column(nullable = false)
+    var originalFileName: String = originalFileName
+        protected set
+
+    companion object {
+        fun defaultProfile(): Profile {
+            return Profile(
+                "profile/default_profile.png",
+                "image/png",
+                "default_profile.png"
+            )
+        }
+    }
+}
