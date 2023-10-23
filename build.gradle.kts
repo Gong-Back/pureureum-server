7c992777f6ae7434a330966f328376cdbcec8755
YC <gks54410@gmail.com>
2023-10-23 21:44:08 +0900

    PUR-24 데이터베이스 연동 작업
    - local / prod / test 환경 Database 구성
    - flyway 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,14 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	runtimeOnly("com.mysql:mysql-connector-j")
+
+	// db
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+	testImplementation("com.h2database:h2:1.4.200")
+
+	// flyway
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-mysql")
 
 	// spring cloud aws + aws parameter store
 	implementation(platform("io.awspring.cloud:spring-cloud-aws-dependencies:3.0.2"))
