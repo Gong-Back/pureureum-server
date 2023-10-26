04059bf4a570f5b8409a6bb291b4f1f7f23b3158
YC <gks54410@gmail.com>
2023-10-26 21:11:16 +0900

    PUR-28 ktlint 설정
    - lint 적용
    - h2 mode MariaDB 지원 버전 문제로 인한 오류 해결

--- src/main/kotlin/gongback/pureureumserver/domain/user/User.kt
+++ src/main/kotlin/gongback/pureureumserver/domain/user/User.kt
@@ -1,11 +1,16 @@
 package gongback.pureureumserver.domain.user
 
-import jakarta.persistence.*
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "users")
 class User(
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L,
 
     val name: String,
