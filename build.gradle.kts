83096e4ae5ff8d5f3d40b3440df86a132a1f8438
jiwon <ljwon77@daum.net>
2023-10-27 20:41:11 +0900

    PUR-9 resolve merge conflict

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.4"
-	id("io.spring.dependency-management") version "1.1.3"
-	kotlin("jvm") version "1.8.22"
-	kotlin("plugin.spring") version "1.8.22"
-	kotlin("plugin.jpa") version "1.8.22"
+    id("org.springframework.boot") version "3.1.4"
+    id("io.spring.dependency-management") version "1.1.3"
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.spring") version "1.8.22"
+    kotlin("plugin.jpa") version "1.8.22"
 }
 
 
@@ -19,48 +20,62 @@ group = "gongback"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-	// db
-	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
-	testImplementation("com.h2database:h2:1.4.200")
+    // db
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+    testImplementation("com.h2database:h2:2.2.222")
 
-	// flyway
-	implementation("org.flywaydb:flyway-core")
-	implementation("org.flywaydb:flyway-mysql")
+    // flyway
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-mysql")
 
-	// spring cloud aws + aws parameter store
-	implementation(platform("io.awspring.cloud:spring-cloud-aws-dependencies:3.0.2"))
-	implementation("io.awspring.cloud:spring-cloud-aws-starter-parameter-store")
-	implementation("org.springframework.cloud:spring-cloud-starter-bootstrap:4.0.4")
+    // spring cloud aws + aws parameter store
+    implementation(platform("io.awspring.cloud:spring-cloud-aws-dependencies:3.0.2"))
+    implementation("io.awspring.cloud:spring-cloud-aws-starter-parameter-store")
+    implementation("org.springframework.cloud:spring-cloud-starter-bootstrap:4.0.4")
 
-	// sentry
-	implementation("io.sentry:sentry-spring-boot-starter-jakarta:6.32.0")
+    // sentry
+    implementation("io.sentry:sentry-spring-boot-starter-jakarta:6.32.0")
 
-	// redis
-	implementation("org.redisson:redisson-spring-boot-starter:3.23.4")
+    // redis
+    implementation("org.redisson:redisson-spring-boot-starter:3.23.4")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // opensearch
+    implementation("org.opensearch.client:spring-data-opensearch-starter:1.2.0") {
+        exclude("org.opensearch.client", "opensearch-rest-client-sniffer")
+    }
+    implementation("org.opensearch.client:spring-data-opensearch-test-autoconfigure:1.2.0") {
+        exclude("org.opensearch.client", "opensearch-rest-client-sniffer")
+    }
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
-	}
-}
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs += "-Xjsr305=strict"
+            jvmTarget = "17"
+        }
+    }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+    ktlint {
+        verbose.set(true)
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+    }
 }
--- src/main/kotlin/gongback/pureureumserver/PureureumServerApplication.kt
+++ src/main/kotlin/gongback/pureureumserver/PureureumServerApplication.kt
@@ -1,11 +1,14 @@
 package gongback.pureureumserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@ConfigurationPropertiesScan
+@SpringBootApplication(exclude = [ElasticsearchDataAutoConfiguration::class])
 class PureureumServerApplication
 
 fun main(args: Array<String>) {
-	runApplication<PureureumServerApplication>(*args)
+    runApplication<PureureumServerApplication>(*args)
 }
