edcaf3073a0920126c56ee0d0c326da635cc5c80
YC <79392304+yeongchan1228@users.noreply.github.com>
2023-10-27 21:15:20 +0900

    PUR-29 API 문서화 작업 (#11)
    - springdoc 연동

--- src/main/kotlin/gongback/pureureumserver/controller/test/RedisTestController.kt
+++ src/main/kotlin/gongback/pureureumserver/controller/test/RedisTestController.kt
@@ -1,10 +1,12 @@
 package gongback.pureureumserver.controller.test
 
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.redisson.api.RedissonClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@Tag(name = "02. Redis Test", description = "레디스 테스트")
 class RedisTestController(
     private val redissonClient: RedissonClient,
 ) {
---
+++ src/main/kotlin/gongback/pureureumserver/support/constant/Securities.kt
@@ -0,0 +1,4 @@
+package gongback.pureureumserver.support.constant
+
+const val BEARER = "Bearer"
+const val JWT = "jwt"
