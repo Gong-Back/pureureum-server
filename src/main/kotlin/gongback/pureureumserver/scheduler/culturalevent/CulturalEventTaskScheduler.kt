5f16c955e601dc55bd32ce83974e4b76480b81bd
YC <gks54410@gmail.com>
2023-11-25 16:07:55 +0900

    PUR-59 락 방식 수정

--- src/main/kotlin/gongback/pureureumserver/scheduler/culturalevent/CulturalEventTaskScheduler.kt
+++ src/main/kotlin/gongback/pureureumserver/scheduler/culturalevent/CulturalEventTaskScheduler.kt
@@ -4,7 +4,7 @@ import gongback.pureureumserver.domain.culturalevent.CulturalEvent
 import gongback.pureureumserver.domain.culturalevent.CulturalEventRepository
 import gongback.pureureumserver.domain.culturalevent.existsByCulturalEventId
 import gongback.pureureumserver.service.CulturalEventClient
-import gongback.pureureumserver.service.LockService
+import gongback.pureureumserver.service.LockTemplate
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
@@ -12,25 +12,29 @@ import org.springframework.transaction.annotation.Transactional
 
 private const val DEFAULT_START_INDEX = 1L
 private const val DEFAULT_INCREASE_VALUE = 10L
-private val log = LoggerFactory.getLogger(CulturalEventTaskScheduler::class.java)
-
 private const val DEFAULT_CULTURAL_EVENT_LOCK_KEY = "syncCulturalEventToDatabase"
+private val log = LoggerFactory.getLogger(CulturalEventTaskScheduler::class.java)
 
 @Service
 class CulturalEventTaskScheduler(
     private val culturalEventClient: CulturalEventClient,
     private val culturalEventRepository: CulturalEventRepository,
-    private val lockService: LockService,
+    private val lockTemplate: LockTemplate,
 ) {
     @Transactional
-    @Scheduled(cron = "0 59 19 * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
     fun syncCulturalEventToDatabase() {
         log.info("[syncCulturalEventToDatabase] start sync cultural event to database")
-        if (!lockService.lock(DEFAULT_CULTURAL_EVENT_LOCK_KEY)) {
-            log.info("[syncCulturalEventToDatabase] already running")
-            return
+        try {
+            lockTemplate.executeWithNoRetry(DEFAULT_CULTURAL_EVENT_LOCK_KEY) {
+                culturalEventSync()
+            }
+        } catch (e: RuntimeException) {
+            log.error("[syncCulturalEventToDatabase] already running")
         }
+    }
 
+    private fun culturalEventSync() {
         try {
             log.info("[syncCulturalEventToDatabase] lock")
 
@@ -56,9 +60,6 @@ class CulturalEventTaskScheduler(
             log.info("[syncCulturalEventToDatabase] end sync cultural event to database")
         } catch (e: Exception) {
             log.error("[syncCulturalEventToDatabase] error occurred while sync cultural event to database", e)
-        } finally {
-            log.info("[syncCulturalEventToDatabase] unlock")
-            lockService.unlock(DEFAULT_CULTURAL_EVENT_LOCK_KEY)
         }
     }
 }
