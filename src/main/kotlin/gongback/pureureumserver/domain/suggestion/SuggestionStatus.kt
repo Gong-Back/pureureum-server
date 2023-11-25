857ee31c5876c59352f2be3671748e04c2de8a63
YC <79392304+yeongchan1228@users.noreply.github.com>
2023-11-25 17:48:25 +0900

    PUR-67 빌드 오류 수정 (#21)
    - 린트 수정
    - 테스트 디비 스키마 추가

--- src/main/kotlin/gongback/pureureumserver/domain/suggestion/SuggestionStatus.kt
+++ src/main/kotlin/gongback/pureureumserver/domain/suggestion/SuggestionStatus.kt
@@ -2,5 +2,5 @@ package gongback.pureureumserver.domain.suggestion
 
 enum class SuggestionStatus(private val description: String) {
     IN_PROGRESS("진행중"),
-    FINISHED("종료");
+    FINISHED("종료"),
 }
