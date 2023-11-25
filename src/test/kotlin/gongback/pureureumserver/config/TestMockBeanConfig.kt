0ea152d7f64845dec97fc591e8706fbe25292080
jiwon <ljwon77@daum.net>
2023-11-25 15:16:51 +0900

    PUR-51 DB schema 업데이트, 테스트 빈 등록

--- src/test/kotlin/gongback/pureureumserver/config/TestMockBeanConfig.kt
+++ src/test/kotlin/gongback/pureureumserver/config/TestMockBeanConfig.kt
@@ -2,6 +2,8 @@ package gongback.pureureumserver.config
 
 import gongback.pureureumserver.infra.culturalevent.SeoulOpenDataClient
 import gongback.pureureumserver.infra.file.AwsS3Client
+import gongback.pureureumserver.security.JwtTokenProvider
+import gongback.pureureumserver.service.FileKeyGenerator
 import org.redisson.api.RedissonClient
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.test.mock.mockito.MockBean
@@ -17,4 +19,10 @@ class TestMockBeanConfig {
 
     @MockBean
     lateinit var seoulOpenDataClient: SeoulOpenDataClient
+
+    @MockBean
+    lateinit var jwtTokenProvider: JwtTokenProvider
+
+    @MockBean
+    lateinit var fileKeyGenerator: FileKeyGenerator
 }
