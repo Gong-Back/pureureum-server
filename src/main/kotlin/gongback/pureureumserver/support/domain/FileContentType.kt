9298bf3640022e49b0a40ef952205f1735208bc6
jiwon <ljwon77@daum.net>
2023-11-26 17:53:25 +0900

    PUR-47 문화 컨텐츠 썸네일 저장 및 조회 시 이미지 URL 반환 추가

--- src/main/kotlin/gongback/pureureumserver/support/domain/FileContentType.kt
+++ src/main/kotlin/gongback/pureureumserver/support/domain/FileContentType.kt
@@ -1,7 +1,20 @@
 package gongback.pureureumserver.support.domain
 
+import java.util.Locale
+
 enum class FileContentType(
     val contentType: String,
 ) {
     SVG("image/svg+xml"),
+    JPG("image/jpeg"),
+    PNG("image/png"),
+    DEFAULT("image/*"),
+
+    ;
+
+    companion object {
+        fun fromExtension(extension: String): FileContentType {
+            return values().find { it.name.lowercase(Locale.getDefault()) == extension } ?: DEFAULT
+        }
+    }
 }
