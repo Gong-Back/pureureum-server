b7b1e3d31e1ce958782af6d9d5ce4049ee43c06c
YC <gks54410@gmail.com>
2023-11-26 18:12:44 +0900

    PUR-69 오타 수정

--- src/main/resources/db/migration/V6__add_cultural_event_thumbnail.sql
+++ src/main/resources/db/migration/V6__add_cultural_event_thumbnail.sql
@@ -1,5 +1,5 @@
 alter table cultural_event
     add column file_id bigint null COMMENT '썸네일 파일 아이디 (FK)';
 
-alter table profile
+alter table cultural_event
     add constraint fk_cultural_event_file_id foreign key (file_id) references file (id);
--- src/test/resources/sql/schema-h2.sql
+++ src/test/resources/sql/schema-h2.sql
@@ -159,5 +159,5 @@ CREATE INDEX idx_suggestion_id_and_user_id_on_suggestion_vote_record ON suggesti
 alter table cultural_event
     add column file_id bigint null COMMENT '썸네일 파일 아이디 (FK)';
 
-alter table profile
+alter table cultural_event
     add constraint fk_cultural_event_file_id foreign key (file_id) references file (id);
